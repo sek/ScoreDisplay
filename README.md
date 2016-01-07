121a290c3f0393e19279ad052aa7c57b8b5bdd03
Stan Kurdziel <sek@stankurdziel.com>
2016-01-07 14:14:01 -0800

    Use LED look font
    - switch to two separate TextViews - one for each character to maintain consistent location for the characters (and preserve the LED look)
    - fix problem with hardware acceleration causing the numbers to disappear if too big for cache
    - scale numbers vertically to better fill screen
    - force portrait orientation, since fonts no longer fit onscreen in landscape

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Two instances of this app running side by side make a single scoreboard.
+
+
+Let's Go Digital Font: http://www.fontspace.com/wlm-fonts/lets-go-digital license: Ubuntu Font License: http://font.ubuntu.com/ufl/
+
--- app/src/main/java/com/stankurdziel/scoredisplay/MainActivity.java
+++ app/src/main/java/com/stankurdziel/scoredisplay/MainActivity.java
@@ -1,67 +1,83 @@
 package com.stankurdziel.scoredisplay;
 
-import android.app.*;
-import android.os.*;
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
-import com.stankurdziel.scoredisplay.MainActivity.*;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	
-	private TextView scoreView;
-	private int score = 0;
 
-	private static String SCORE = "SCORE";
+    private TextView scoreL;
+    private TextView scoreR;
+
+    private int score = 0;
+    private static String SCORE = "SCORE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-		scoreView = (TextView) findViewById(R.id.score);
-		if(savedInstanceState != null) {
-			score = savedInstanceState.getInt(SCORE, 0);
-			updateUi();
-		}
+        setContentView(R.layout.main);
 
-		View.OnClickListener onClick = new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				switch(v.getId()) {
-					case R.id.down:
-						if (score>0) score--;
-					    break;
-					case R.id.up:
-						score++;
-						break;
-				}
-				updateUi();
-			}
-		};
-		findViewById(R.id.down).setOnClickListener(onClick);
-		findViewById(R.id.up).setOnClickListener(onClick);
+        scoreL = (TextView) findViewById(R.id.scoreL);
+        scoreR = (TextView) findViewById(R.id.scoreR);
+        final Typeface font = Typeface.createFromAsset(getAssets(), "Let's go Digital Regular.ttf");
+        scoreL.setTypeface(font);
+        scoreR.setTypeface(font);
+        scoreL.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        scoreR.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getInt(SCORE, 0);
+            updateUi();
+        }
+
+        View.OnClickListener onClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.down:
+                        if (score > 0) score--;
+                        break;
+                    case R.id.up:
+                        if (score < 99) score++;
+                        break;
+                }
+                updateUi();
+            }
+        };
+        findViewById(R.id.down).setOnClickListener(onClick);
+        findViewById(R.id.up).setOnClickListener(onClick);
     }
 
-	
-	private void updateUi() {
-		scoreView.setText(String.valueOf(score));
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		getWindow().getDecorView().setSystemUiVisibility(
-			View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-			| View.SYSTEM_UI_FLAG_FULLSCREEN
-			| View.SYSTEM_UI_FLAG_IMMERSIVE);
-	}
 
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		outState.putInt(SCORE, score);
-		super.onSaveInstanceState(outState);
-	}
+    private void updateUi() {
+        setScore(score);
+    }
+
+    private void setScore(int score) {
+        String leftCharacter = "";
+        if (score / 10 != 0) leftCharacter = String.valueOf(score / 10);
+        scoreL.setText(leftCharacter);
+        final String rightCharacter = String.valueOf(score % 10);
+        scoreR.setText(rightCharacter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SCORE, score);
+        super.onSaveInstanceState(outState);
+    }
 }
